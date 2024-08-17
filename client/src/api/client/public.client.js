d29248469d6a13c3a4ba2a4fb0210303fb2c2f09
GaurangPatel <148003048+GaurangPatel13@users.noreply.github.com>
2024-08-17 16:23:59 +0530

    Update public.client.js

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://localhost:3000/";
+const baseURL = "https://entertainment-app-mitt.onrender.com";
 
 const publicClient = axios.create({
   baseURL,
