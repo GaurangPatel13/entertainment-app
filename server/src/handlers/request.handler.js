9f9d9763e1ef0eda988a6145fcd614fe07535268
GaurangPatel <148003048+GaurangPatel13@users.noreply.github.com>
2024-08-31 10:52:08 +0530

    Update request.handler.js

--- server/src/handlers/request.handler.js
+++ server/src/handlers/request.handler.js
@@ -1,13 +1,15 @@
-import { validationResult } from "express-validator";
+import { validationResult } from "express-validator"; // Importing the validationResult function from express-validator package.
 
-const validate = (req, res, next) => {
-  const errors = validationResult(req);
+  const validate = (req, res, next) => { // Middleware function to handle validation results.
+  const errors = validationResult(req); // Extract validation errors from the request.
+  
+  if (!errors.isEmpty()) return res.status(400).json({ // If there are validation errors, respond with a 400 status code and the first error message.
 
-  if (!errors.isEmpty()) return res.status(400).json({
-    message: errors.array()[0].msg
+  message: errors.array()[0].msg // Send the first validation error message in the response.
   });
+    
+  next(); // If no validation errors, proceed to the next middleware or route handler.
+  };
 
-  next();
-};
-
-export default { validate };
+export default { validate }; 
+// Exporting the validate function as part of an object for use in other parts of the application.
